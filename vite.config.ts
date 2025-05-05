9fdab9087398e2063d346cc452b65f24fe049329
suraj <surajmahato9823@gmail.com>
2025-05-05 22:36:22 +0545

    Update project

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/rebazzar/', // Replace with your repository name
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
