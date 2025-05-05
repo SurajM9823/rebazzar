0309e07619ded9351dcdcab1e627d667a36faa9e
SurajM9823 <surajmahato9823@gmail.com>
2025-05-05 22:47:09 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/rebazzar/', // Replace with your repository name
+  base: '/rebazzar/',
   plugins: [react()],
 });
